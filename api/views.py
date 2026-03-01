e1236ba8d88eb54e4f8aa822ce93be6036f65b90
Frimpong Joseph <josephfrimpong91@gmail.com>
2026-03-01 11:43:02 +0000

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]   # 👈 VERY IMPORTANT
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        email = request.data.get("email")
+
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email
+        )
+
+        return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
